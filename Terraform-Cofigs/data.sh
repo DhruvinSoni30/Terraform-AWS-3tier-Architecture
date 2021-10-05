e3c86dc4ebc3b787e3b22bb9897ebb87b2c23baa
Dhruvin Soni <dhruvinsoni30@gmail.com>
2021-10-05 15:48:20 +0530

    Adding code

--- Terraform-Cofigs/data.sh
+++ Terraform-Cofigs/data.sh
@@ -4,4 +4,4 @@ yum update -y
 yum install -y httpd.x86_64
 systemctl start httpd.service
 systemctl enable httpd.service
-echo "Hello World from Dhruvin Soni" > /var/www/html/index.html
+echo "Hello World from $(hostname -f)" > /var/www/html/index.html
